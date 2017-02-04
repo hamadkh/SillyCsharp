20b7d035644877641d54e8affce4df5acd70d5b5
hamad Khawaja <khawajahamad217@gmail.com>
2017-02-03 20:03:52 -0500

    punchline finish

--- JokePunchline/JokePunchline/Form1.Designer.cs
+++ JokePunchline/JokePunchline/Form1.Designer.cs
@@ -28,21 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSetup = new System.Windows.Forms.Button();
+            this.btnPunchline = new System.Windows.Forms.Button();
+            this.lblText = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnSetup
+            // 
+            this.btnSetup.Location = new System.Drawing.Point(95, 110);
+            this.btnSetup.Name = "btnSetup";
+            this.btnSetup.Size = new System.Drawing.Size(75, 23);
+            this.btnSetup.TabIndex = 0;
+            this.btnSetup.Text = "button1";
+            this.btnSetup.UseVisualStyleBackColor = true;
+            this.btnSetup.Click += new System.EventHandler(this.btnSetup_Click);
+            // 
+            // btnPunchline
+            // 
+            this.btnPunchline.Location = new System.Drawing.Point(344, 110);
+            this.btnPunchline.Name = "btnPunchline";
+            this.btnPunchline.Size = new System.Drawing.Size(75, 23);
+            this.btnPunchline.TabIndex = 1;
+            this.btnPunchline.Text = "button2";
+            this.btnPunchline.UseVisualStyleBackColor = true;
+            this.btnPunchline.Click += new System.EventHandler(this.btnPunchline_Click);
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(92, 42);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(35, 13);
+            this.lblText.TabIndex = 2;
+            this.lblText.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(576, 182);
+            this.ClientSize = new System.Drawing.Size(531, 182);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.btnPunchline);
+            this.Controls.Add(this.btnSetup);
             this.Name = "Form1";
             this.RightToLeftLayout = true;
             this.Text = "Joke & Punchline ";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSetup;
+        private System.Windows.Forms.Button btnPunchline;
+        private System.Windows.Forms.Label lblText;
     }
 }
 
--- JokePunchline/JokePunchline/Form1.cs
+++ JokePunchline/JokePunchline/Form1.cs
@@ -16,5 +16,22 @@ namespace JokePunchline
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblText.Text = "";
+            btnSetup.Text = "Setup";
+            btnPunchline.Text = "Punchline";
+        }
+
+        private void btnSetup_Click(object sender, EventArgs e)
+        {
+            lblText.Text = "What's the Object-Oriented way to become wealthy ?";
+        }
+
+        private void btnPunchline_Click(object sender, EventArgs e)
+        {
+            lblText.Text = "Inheritance";
+        }
     }
 }
