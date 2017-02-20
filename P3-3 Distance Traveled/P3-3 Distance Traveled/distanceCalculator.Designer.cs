2de73781255a8af1b852707f6e4ceed6cebf56e5
hamad Khawaja <khawajahamad217@gmail.com>
2017-02-19 23:58:58 -0500

    distanceCalc

---
+++ P3-3 Distance Traveled/P3-3 Distance Traveled/distanceCalculator.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace P3_3_Distance_Traveled
+{
+    partial class distanceCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMiles = new System.Windows.Forms.Label();
+            this.txtMiles = new System.Windows.Forms.TextBox();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.lblDistance = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.Label();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMiles
+            // 
+            this.lblMiles.AutoSize = true;
+            this.lblMiles.Location = new System.Drawing.Point(31, 37);
+            this.lblMiles.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMiles.Name = "lblMiles";
+            this.lblMiles.Size = new System.Drawing.Size(187, 16);
+            this.lblMiles.TabIndex = 0;
+            this.lblMiles.Text = "Enter Speed in Miles Per Hour";
+            // 
+            // txtMiles
+            // 
+            this.txtMiles.Location = new System.Drawing.Point(303, 31);
+            this.txtMiles.Name = "txtMiles";
+            this.txtMiles.Size = new System.Drawing.Size(115, 22);
+            this.txtMiles.TabIndex = 1;
+            // 
+            // txtHours
+            // 
+            this.txtHours.Location = new System.Drawing.Point(303, 82);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(115, 22);
+            this.txtHours.TabIndex = 3;
+            // 
+            // lblHours
+            // 
+            this.lblHours.AutoSize = true;
+            this.lblHours.Location = new System.Drawing.Point(31, 88);
+            this.lblHours.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(264, 16);
+            this.lblHours.TabIndex = 2;
+            this.lblHours.Text = "Enter Number of Hours (in Whole Numbers)";
+            // 
+            // lblDistance
+            // 
+            this.lblDistance.AutoSize = true;
+            this.lblDistance.Location = new System.Drawing.Point(31, 139);
+            this.lblDistance.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDistance.Name = "lblDistance";
+            this.lblDistance.Size = new System.Drawing.Size(153, 16);
+            this.lblDistance.TabIndex = 4;
+            this.lblDistance.Text = "Total Distance Traveled";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtTotal.Location = new System.Drawing.Point(303, 125);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(115, 30);
+            this.txtTotal.TabIndex = 5;
+            // 
+            // btnCal
+            // 
+            this.btnCal.Location = new System.Drawing.Point(34, 189);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(150, 40);
+            this.btnCal.TabIndex = 6;
+            this.btnCal.Text = "Calculate Distance";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(190, 189);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(111, 40);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(307, 189);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(111, 40);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // distanceCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 259);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCal);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.lblDistance);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.lblHours);
+            this.Controls.Add(this.txtMiles);
+            this.Controls.Add(this.lblMiles);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "distanceCalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Distance Traveled";
+            this.Load += new System.EventHandler(this.distanceCalculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMiles;
+        private System.Windows.Forms.TextBox txtMiles;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.Label lblHours;
+        private System.Windows.Forms.Label lblDistance;
+        private System.Windows.Forms.Label txtTotal;
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ P3-3 Distance Traveled/P3-3 Distance Traveled/distanceCalculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace P3_3_Distance_Traveled
+{
+    public partial class distanceCalculator : Form
+    {
+        public distanceCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void distanceCalculator_Load(object sender, EventArgs e)
+        {
+            this.AcceptButton = this.btnCal;
+            this.CancelButton = this.btnExit;
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            double speed, hours,distance;
+
+            try
+            {
+                speed = double.Parse(txtMiles.Text);
+                hours = double.Parse(txtHours.Text);
+
+
+                distance = distanceCalc(speed, hours);
+
+                txtTotal.Text = distance.ToString("0.0");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please enter a proper value", ex.Message);
+                if (!double.TryParse(txtHours.Text, out hours))
+                {
+                    txtHours.Select();
+                    return;
+                }
+                
+                 else if (!double.TryParse(txtMiles.Text, out speed))
+                    {
+                        txtMiles.Select();
+                        return;
+                    }
+                
+            }
+           
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtHours.Text = "";
+            txtMiles.Text = "";
+            txtTotal.Text = "";
+        }
+
+        double distanceCalc(double speed, double hours)
+        {
+            return speed * hours;
+        }
+    }
+}
