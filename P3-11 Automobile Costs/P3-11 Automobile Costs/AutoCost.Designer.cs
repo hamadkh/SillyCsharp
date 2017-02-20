824ab0d7d7cb3368df5092cecfbbe58f38436fc2
hamad Khawaja <khawajahamad217@gmail.com>
2017-02-20 01:27:12 -0500

    last question

---
+++ P3-11 Automobile Costs/P3-11 Automobile Costs/AutoCost.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace P3_11_Automobile_Costs
+{
+    partial class AutoCost
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtAnnualTotal = new System.Windows.Forms.Label();
+            this.txtMonthlyTotal = new System.Windows.Forms.Label();
+            this.lblAtotal = new System.Windows.Forms.Label();
+            this.lblMtotal = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtMaintenance = new System.Windows.Forms.TextBox();
+            this.txtTires = new System.Windows.Forms.TextBox();
+            this.txtOil = new System.Windows.Forms.TextBox();
+            this.txtGas = new System.Windows.Forms.TextBox();
+            this.txtInsurance = new System.Windows.Forms.TextBox();
+            this.txtLoan = new System.Windows.Forms.TextBox();
+            this.lblMaintenance = new System.Windows.Forms.Label();
+            this.lblTires = new System.Windows.Forms.Label();
+            this.lblOil = new System.Windows.Forms.Label();
+            this.lblGas = new System.Windows.Forms.Label();
+            this.lblInsurance = new System.Windows.Forms.Label();
+            this.lblLoan = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(372, 358);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(5);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(133, 34);
+            this.btnExit.TabIndex = 38;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(372, 303);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(5);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(133, 34);
+            this.btnReset.TabIndex = 37;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // txtAnnualTotal
+            // 
+            this.txtAnnualTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtAnnualTotal.Location = new System.Drawing.Point(183, 354);
+            this.txtAnnualTotal.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.txtAnnualTotal.Name = "txtAnnualTotal";
+            this.txtAnnualTotal.Size = new System.Drawing.Size(177, 39);
+            this.txtAnnualTotal.TabIndex = 36;
+            // 
+            // txtMonthlyTotal
+            // 
+            this.txtMonthlyTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtMonthlyTotal.Location = new System.Drawing.Point(183, 303);
+            this.txtMonthlyTotal.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.txtMonthlyTotal.Name = "txtMonthlyTotal";
+            this.txtMonthlyTotal.Size = new System.Drawing.Size(177, 34);
+            this.txtMonthlyTotal.TabIndex = 35;
+            // 
+            // lblAtotal
+            // 
+            this.lblAtotal.AutoSize = true;
+            this.lblAtotal.Location = new System.Drawing.Point(32, 354);
+            this.lblAtotal.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblAtotal.Name = "lblAtotal";
+            this.lblAtotal.Size = new System.Drawing.Size(83, 16);
+            this.lblAtotal.TabIndex = 34;
+            this.lblAtotal.Text = "Annual Total";
+            // 
+            // lblMtotal
+            // 
+            this.lblMtotal.AutoSize = true;
+            this.lblMtotal.Location = new System.Drawing.Point(32, 303);
+            this.lblMtotal.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblMtotal.Name = "lblMtotal";
+            this.lblMtotal.Size = new System.Drawing.Size(88, 16);
+            this.lblMtotal.TabIndex = 33;
+            this.lblMtotal.Text = "Monthly Total";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(372, 233);
+            this.btnCalc.Margin = new System.Windows.Forms.Padding(5);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(133, 34);
+            this.btnCalc.TabIndex = 32;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtMaintenance
+            // 
+            this.txtMaintenance.Location = new System.Drawing.Point(183, 238);
+            this.txtMaintenance.Margin = new System.Windows.Forms.Padding(5);
+            this.txtMaintenance.Name = "txtMaintenance";
+            this.txtMaintenance.Size = new System.Drawing.Size(175, 22);
+            this.txtMaintenance.TabIndex = 31;
+            // 
+            // txtTires
+            // 
+            this.txtTires.Location = new System.Drawing.Point(183, 197);
+            this.txtTires.Margin = new System.Windows.Forms.Padding(5);
+            this.txtTires.Name = "txtTires";
+            this.txtTires.Size = new System.Drawing.Size(175, 22);
+            this.txtTires.TabIndex = 30;
+            // 
+            // txtOil
+            // 
+            this.txtOil.Location = new System.Drawing.Point(183, 158);
+            this.txtOil.Margin = new System.Windows.Forms.Padding(5);
+            this.txtOil.Name = "txtOil";
+            this.txtOil.Size = new System.Drawing.Size(175, 22);
+            this.txtOil.TabIndex = 29;
+            // 
+            // txtGas
+            // 
+            this.txtGas.Location = new System.Drawing.Point(183, 116);
+            this.txtGas.Margin = new System.Windows.Forms.Padding(5);
+            this.txtGas.Name = "txtGas";
+            this.txtGas.Size = new System.Drawing.Size(175, 22);
+            this.txtGas.TabIndex = 28;
+            // 
+            // txtInsurance
+            // 
+            this.txtInsurance.Location = new System.Drawing.Point(183, 75);
+            this.txtInsurance.Margin = new System.Windows.Forms.Padding(5);
+            this.txtInsurance.Name = "txtInsurance";
+            this.txtInsurance.Size = new System.Drawing.Size(175, 22);
+            this.txtInsurance.TabIndex = 27;
+            // 
+            // txtLoan
+            // 
+            this.txtLoan.Location = new System.Drawing.Point(183, 34);
+            this.txtLoan.Margin = new System.Windows.Forms.Padding(5);
+            this.txtLoan.Name = "txtLoan";
+            this.txtLoan.Size = new System.Drawing.Size(175, 22);
+            this.txtLoan.TabIndex = 26;
+            // 
+            // lblMaintenance
+            // 
+            this.lblMaintenance.AutoSize = true;
+            this.lblMaintenance.Location = new System.Drawing.Point(32, 242);
+            this.lblMaintenance.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblMaintenance.Name = "lblMaintenance";
+            this.lblMaintenance.Size = new System.Drawing.Size(88, 16);
+            this.lblMaintenance.TabIndex = 25;
+            this.lblMaintenance.Text = "Maintenance:";
+            // 
+            // lblTires
+            // 
+            this.lblTires.AutoSize = true;
+            this.lblTires.Location = new System.Drawing.Point(32, 201);
+            this.lblTires.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblTires.Name = "lblTires";
+            this.lblTires.Size = new System.Drawing.Size(42, 16);
+            this.lblTires.TabIndex = 24;
+            this.lblTires.Text = "Tires:";
+            // 
+            // lblOil
+            // 
+            this.lblOil.AutoSize = true;
+            this.lblOil.Location = new System.Drawing.Point(33, 161);
+            this.lblOil.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblOil.Name = "lblOil";
+            this.lblOil.Size = new System.Drawing.Size(27, 16);
+            this.lblOil.TabIndex = 23;
+            this.lblOil.Text = "Oil:";
+            // 
+            // lblGas
+            // 
+            this.lblGas.AutoSize = true;
+            this.lblGas.Location = new System.Drawing.Point(32, 121);
+            this.lblGas.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblGas.Name = "lblGas";
+            this.lblGas.Size = new System.Drawing.Size(36, 16);
+            this.lblGas.TabIndex = 22;
+            this.lblGas.Text = "Gas:";
+            // 
+            // lblInsurance
+            // 
+            this.lblInsurance.AutoSize = true;
+            this.lblInsurance.Location = new System.Drawing.Point(33, 80);
+            this.lblInsurance.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblInsurance.Name = "lblInsurance";
+            this.lblInsurance.Size = new System.Drawing.Size(69, 16);
+            this.lblInsurance.TabIndex = 21;
+            this.lblInsurance.Text = "Insurance:";
+            // 
+            // lblLoan
+            // 
+            this.lblLoan.AutoSize = true;
+            this.lblLoan.Location = new System.Drawing.Point(33, 34);
+            this.lblLoan.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblLoan.Name = "lblLoan";
+            this.lblLoan.Size = new System.Drawing.Size(96, 16);
+            this.lblLoan.TabIndex = 20;
+            this.lblLoan.Text = "Loan payment:";
+            // 
+            // AutoCost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 425);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtAnnualTotal);
+            this.Controls.Add(this.txtMonthlyTotal);
+            this.Controls.Add(this.lblAtotal);
+            this.Controls.Add(this.lblMtotal);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtMaintenance);
+            this.Controls.Add(this.txtTires);
+            this.Controls.Add(this.txtOil);
+            this.Controls.Add(this.txtGas);
+            this.Controls.Add(this.txtInsurance);
+            this.Controls.Add(this.txtLoan);
+            this.Controls.Add(this.lblMaintenance);
+            this.Controls.Add(this.lblTires);
+            this.Controls.Add(this.lblOil);
+            this.Controls.Add(this.lblGas);
+            this.Controls.Add(this.lblInsurance);
+            this.Controls.Add(this.lblLoan);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "AutoCost";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Automobile Costs Calculator";
+            this.Load += new System.EventHandler(this.AutoCost_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label txtAnnualTotal;
+        private System.Windows.Forms.Label txtMonthlyTotal;
+        private System.Windows.Forms.Label lblAtotal;
+        private System.Windows.Forms.Label lblMtotal;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtMaintenance;
+        private System.Windows.Forms.TextBox txtTires;
+        private System.Windows.Forms.TextBox txtOil;
+        private System.Windows.Forms.TextBox txtGas;
+        private System.Windows.Forms.TextBox txtInsurance;
+        private System.Windows.Forms.TextBox txtLoan;
+        private System.Windows.Forms.Label lblMaintenance;
+        private System.Windows.Forms.Label lblTires;
+        private System.Windows.Forms.Label lblOil;
+        private System.Windows.Forms.Label lblGas;
+        private System.Windows.Forms.Label lblInsurance;
+        private System.Windows.Forms.Label lblLoan;
+    }
+}
+
---
+++ P3-11 Automobile Costs/P3-11 Automobile Costs/AutoCost.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P3_11_Automobile_Costs
+{
+    public partial class AutoCost : Form
+    {
+        public AutoCost()
+        {
+            InitializeComponent();
+        }
+
+        private void AutoCost_Load(object sender, EventArgs e)
+        {
+            this.AcceptButton = this.btnCalc;
+            this.CancelButton = this.btnExit;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtAnnualTotal.Text = "";
+            txtGas.Text = "";
+            txtInsurance.Text = "";
+            txtLoan.Text = "";
+            txtMaintenance.Text = "";
+            txtMonthlyTotal.Text = "";
+            txtOil.Text = "";
+            txtTires.Text = "";
+
+            txtLoan.Select(); 
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private double Cost(double loan, double insurance, double gas, double oil, double tires, double maintenance)
+        {
+
+            return loan + insurance + gas + oil + tires + maintenance;
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double loan, insurance,gas, oil, tires, maintenance, monthlyTotal, annualTotal;
+
+            try
+            {
+                loan = double.Parse(txtLoan.Text);
+                insurance = double.Parse(txtInsurance.Text);
+                gas = double.Parse(txtGas.Text);
+                oil = double.Parse(txtOil.Text);
+                tires = double.Parse(txtTires.Text);
+                maintenance = double.Parse(txtMaintenance.Text);
+
+                monthlyTotal = Cost(loan, insurance, gas, oil, tires, maintenance);
+                annualTotal = 12*Cost(loan, insurance, gas, oil, tires, maintenance);
+
+                txtMonthlyTotal.Text = monthlyTotal.ToString("#.##");
+                txtAnnualTotal.Text = annualTotal.ToString("#.##");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please enter a proper value", ex.Message);
+                if (!double.TryParse(txtGas.Text, out gas))
+                {
+                    txtGas.Select();
+                    return;
+                }
+
+                if (!double.TryParse(txtInsurance.Text, out insurance))
+                {
+                    txtInsurance.Select();
+                    return;
+                }
+
+                if (!double.TryParse(txtLoan.Text, out loan))
+                {
+                    txtLoan.Select();
+                    return;
+                }
+
+                if (!double.TryParse(txtMaintenance.Text, out maintenance))
+                {
+                    txtMaintenance.Select();
+                    return;
+                }
+
+                if (!double.TryParse(txtOil.Text, out oil))
+                {
+                    txtOil.Select();
+                    return;
+                }
+
+                if (!double.TryParse(txtTires.Text, out tires))
+                {
+                    txtTires.Select();
+                    return;
+                }
+            }
+        }
+    }
+}
