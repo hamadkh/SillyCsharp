e384f1c3e102717b7c9019097dfad1ee7cac7686
hamad Khawaja <khawajahamad217@gmail.com>
2017-03-11 00:40:29 -0500

    last

--- P4-8 BMI Enhancement/P4-8 BMI Enhancement/BMI.Designer.cs
+++ P4-8 BMI Enhancement/P4-8 BMI Enhancement/BMI.Designer.cs
@@ -28,71 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button3
+            // btnExit
             // 
-            this.button3.Location = new System.Drawing.Point(509, 159);
-            this.button3.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(156, 42);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "E&xit";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnExit.Location = new System.Drawing.Point(509, 159);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(156, 42);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
-            // button2
+            // btnReset
             // 
-            this.button2.Location = new System.Drawing.Point(297, 159);
-            this.button2.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(156, 42);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "&Reset";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnReset.Location = new System.Drawing.Point(297, 159);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(156, 42);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
-            // button1
+            // btnCalc
             // 
-            this.button1.Location = new System.Drawing.Point(53, 159);
-            this.button1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(188, 42);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "&Calculate BMI";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnCalc.Location = new System.Drawing.Point(53, 159);
+            this.btnCalc.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(188, 42);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "&Calculate BMI";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
             // 
-            // label3
+            // lblResult
             // 
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(332, 38);
-            this.label3.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(334, 85);
-            this.label3.TabIndex = 22;
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(332, 38);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(334, 85);
+            this.lblResult.TabIndex = 22;
             // 
-            // textBox2
+            // txtHeight
             // 
-            this.textBox2.Location = new System.Drawing.Point(179, 101);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(118, 22);
-            this.textBox2.TabIndex = 1;
+            this.txtHeight.Location = new System.Drawing.Point(179, 101);
+            this.txtHeight.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(118, 22);
+            this.txtHeight.TabIndex = 1;
             // 
-            // textBox1
+            // txtWeight
             // 
-            this.textBox1.Location = new System.Drawing.Point(179, 36);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(118, 22);
-            this.textBox1.TabIndex = 0;
+            this.txtWeight.Location = new System.Drawing.Point(179, 36);
+            this.txtWeight.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(118, 22);
+            this.txtWeight.TabIndex = 0;
             // 
             // label2
             // 
@@ -114,23 +117,23 @@
             this.label1.TabIndex = 18;
             this.label1.Text = "Weight (pounds):";
             // 
-            // Form1
+            // BMI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(724, 240);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWeight);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "Form1";
+            this.Name = "BMI";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BMI Calculator";
             this.ResumeLayout(false);
@@ -140,12 +143,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWeight;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
     }
--- P4-8 BMI Enhancement/P4-8 BMI Enhancement/BMI.cs
+++ P4-8 BMI Enhancement/P4-8 BMI Enhancement/BMI.cs
@@ -16,5 +16,64 @@ namespace P4_8_BMI_Enhancement
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtHeight.Text = "";
+            txtWeight.Text = "";
+            lblResult.Text = "";
+            txtWeight.Focus();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double weight, height, BMI;
+
+            try
+            {
+                weight = double.Parse(txtWeight.Text);
+                height = double.Parse(txtHeight.Text);
+
+                if ((weight >= 40 && weight <= 500) && (height >= 20 && height <= 108))
+                {
+                    BMI = BMICalc(weight, height);
+                    if (BMI >= 18.5 && BMI <= 25)
+                    {
+                        lblResult.Text = BMI.ToString("0.00" + Environment.NewLine + "Your BMI is Optimal");
+                    }
+
+                    if (BMI < 18.5)
+                    {
+                        lblResult.Text = BMI.ToString("0.00" + Environment.NewLine + "Underweight");
+                    }
+
+                    if (BMI > 25)
+                    {
+                        lblResult.Text = BMI.ToString("0.00" + Environment.NewLine + "Overweight");
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Enter Correct Weight/Height range");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Enter a correct Value",ex.Message);
+            }
+           
+        }
+
+        private double BMICalc(double w, double h)
+        {
+            return w * 703 / (h*h);
+        }
     }
 }
